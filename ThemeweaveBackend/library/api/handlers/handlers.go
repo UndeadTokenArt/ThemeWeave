72f9ef8dcc39d0440f0816e2ca0909a94a784f6e
Stephen Moore <UndeadTokenArt@gmail.com>
2025-07-23 10:57:27 -0700

    working on solving how to get the peices all to come together

--- ThemeweaveBackend/library/api/handlers/handlers.go
+++ ThemeweaveBackend/library/api/handlers/handlers.go
@@ -7,7 +7,13 @@ import (
 )
 
 func HandleIndex(c *gin.Context) {
+	firstE := []string{"element1", "element2", "element3"}
+	secE := []string{"itemA", "itemB", "itemC"}
+	olList := [][]string{firstE, secE}
+
 	c.HTML(http.StatusOK, "WebInterface.tmpl", gin.H{
-		"Title": "Welcome to the ThemeWeave API!",
+		"Title":        "Heirarchy List",
+		"Message":      "Welcome to ThemeWeave!",
+		"Heirchierchy": olList,
 	})
 }
